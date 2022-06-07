02c26f71163792ce29bb62319ea4b50c2f085063
pabloberry44 <pawel.borowiec@tutanota.com>
2022-06-07 14:38:02 +0200

    listing elements matching user input

--- script.js
+++ script.js
@@ -10,19 +10,28 @@ let amountInpt = document.querySelectorAll('.data')[0]
 let fromInpt = document.querySelectorAll('.data')[1]
 let toInpt = document.querySelectorAll('.data')[2]
 const submit = document.querySelector('.submit')
+const list = document.querySelector('.list')
 
 const currency = ['AED','AFN','ALL','AMD','ANG','AOA','ARS','AUD','AWG','AZN','BAM','BBD','BDT','BGN','BHD','BIF','BMD','BND','BOB','BRL','BSD','BTC','BTN','BWP','BYN','BYR','BZD','CAD','CDF','CHF','CLF','CLP','CNY','COP','CRC','CUC','CUP','CVE','CZK','DJF','DKK','DOP','DZD','EGP','ERN','ETB','EUR','FJD','FKP','GBP','GEL','GGP','GHS','GIP','GMD','GNF','GTQ','GYD','HKD','HNL','HRK','HTG','HUF','IDR','ILS','IMP','INR','IQD','IRR','ISK','JEP','JMD','JOD','JPY','KES','KGS','KHR','KMF','KPW','KRW','KWD','KYD','KZT','LAK','LBP','LKR','LRD','LSL','LVL','LYD','MAD','MDL','MGA','MKD','MMK','MNT','MOP','MRO','MUR','MVR','MWK','MXN','MYR','MZN','NAD','NGN','NIO','NOK','NPR','NZD','OMR','PAB','PEN','PGK','PHP','PKR','PLN','PYG','QAR','RON','RSD','RUB','RWF','SAR','SBD','SCR','SDG','SEK','SGD','SHP','SLL','SOS','SRD','STD','SVC','SYP','SZL','THB','TJS','TMT','TND','TOP','TRY','TTD','TWD','TZS','UAH','UGX','USD','UYU','UZS','VEF','VND','VUV','WST','XAF','XAG','XCD','XDR','XOF','XPF','YER','ZAR','ZMK','ZMW','ZWL']
 
 // submit.addEventListener('click', pass_to_api(fromInpt.value, toInpt.value, amountInpt.value))
 
 fromInpt.addEventListener('input', ()=>{
-    console.log('input')
 
-    for(i=0; i<currency.length; i++){
-        if(currency[i].startsWith(fromInpt.value)){
-            console.log(currency[i])
-        }
+    const listElements = list.children
+
+    while (list.firstChild) {
+      list.removeChild(list.lastChild);
     }
+
+    currency.forEach(currency => {
+        if(currency.startsWith(fromInpt.value)){
+            const node = document.createElement('p');
+            const textnode = document.createTextNode(currency);
+            node.appendChild(textnode);
+            list.appendChild(node);
+        }
+    });
 })
 
 // function pass_to_api(have, want, amount){
